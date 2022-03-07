a2d1f76bc2faad288a7f849b882f2cd999cf7326
Umer Farooq <umerbaba86@gmail.com>
2022-03-07 09:56:37 +0000

    leetcode longest substring Without Repeating Characters added

--- paintArrayCodeforces.cpp
+++ paintArrayCodeforces.cpp
@@ -11,19 +11,31 @@ int t;
 cin>>t;
 while(t--){ 
     
-    int n;
-    cin>>n;
-    long long int arr[n];
-    int max_val =INT_MIN;
-    int min_val =  INT_MAX;
-    for(long long int i=0;i<n;i++){
-    	cin>>arr[i];
-    	max_val =max(arr[i],max_val);
-    	min_val = min(arr[i],min_val);
+    int arr[3];
+    int min_val = INT_MAX;
+    int max_val = INT_MIN;
+    int min_index,max_index;
+    for(int i=0;i<3;i++){
+      cin>>arr[i];
+      min_val = min(arr[i],min_val);
+      if(arr[i]<min_val){
+      	min_index= i;
+      }
+      if(arr[i]> max_val){
+      	max_index = i;
+      }
+    }
+    int t_index;
+    for(int i=0;i<3;i++){
+    	if(i !=max_index && i!=min_index ){
+    		t_index =i;
+    	}
     }
 
-    long long int ans = max_val - min_val;
-    cout<<ans<<endl;
+    for(int i=1;i<max_val;i++){
+    	arr[t_index] = arr[t_index]*1;
+    	gcd()
+    }
      
 }
 
